84eb79ab451bd02ad91485c16eb1de50f7c39c24
Max Worsley <maxworsley@outlook.com>
2024-02-15 08:04:55 +0000

    Add python test

---
+++ multiprocess-py/multiprocess_test.py
@@ -0,0 +1,25 @@
+from multiprocessing import Pool
+import timeit
+
+ITERATIONS_OF_WORK = 8
+
+def busy_work(input):
+    x = 0
+    for i in range(1_000_0000):
+        x += i
+    return x
+
+def test_multi():
+    pool = Pool(processes=16)
+    results = pool.map(busy_work, range(ITERATIONS_OF_WORK))
+
+def test_single():
+    for i in range(ITERATIONS_OF_WORK):
+        busy_work(i)
+
+if __name__ == '__main__':
+    test_iterations = 10
+    print("Multi: ", end="")
+    print(timeit.timeit("test_multi()", setup="from __main__ import test_multi", number=test_iterations))
+    print("Single: ", end="")
+    # print(timeit.timeit("test_single()", setup="from __main__ import test_single", number=test_iterations))
